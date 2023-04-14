a851f1f0b52bb63ab823ae05b0ccee551179808b
alarconOrtiz-Diego <diegoalarcondrum@gmail.com>
2023-04-14 14:35:53 +0200

    responsiveAndSomeJS

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 window.onload = () => {
-    // Contenido que se ejecuta
+    document.getElementById("addLista").onclick = () => {
+        crearLista("nombre", "ink")
+    }
 }
 
 // FUNCIONES UTILIZADAS
 
-// ...
+function crearLista(nombre, color) {
+    if (nombre == "ftuser") {
+        // Cositas
+    } else {
+        let lista = document.createElement("div");
+        lista.setAttribute("class", "lista");
+        let h5 = document.createElement("h5");
+        h5.textContent = nombre;
+        let cantidad = document.createElement("div");
+        cantidad.setAttribute("class", "cantidadElementosLista")
+        cantidad.textContent = "0";
+        lista.appendChild(h5);
+        lista.appendChild(cantidad);
+        lista.style.backgroundColor = color;
+        document.getElementById("contenedorListas").appendChild(lista);
+    }
+}
